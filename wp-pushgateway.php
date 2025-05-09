517ebc5fbe7a489c999bebfdb46c988ebb313c35
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-05-09 16:51:22 +0200

    [init] Blank PHP script for a WordPress plugin

---
+++ wp-pushgateway.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WordPress Pushgateway
+ * Description: Push metrics to a Prometheus pushgateway in a wp-cron task
+ * Version: 0.1
+ * Author: ISAS-FSD <isas-fsd@groupes.epfl.ch>
+ * License: Copyright (c) 2025 Ecole Polytechnique Federale de Lausanne, Switzerland
+ **/
+
